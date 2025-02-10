aee48f0f25630b6825f0d5445f962afb3bc7e34e
Derick Fynn <dcfynn@icloud.com>
2025-02-10 05:24:42 +0200

    feat(config): uncommented output option

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 const basePath = isProd ? "/tic-tac-toe" : "";
 const nextConfig: NextConfig = {
-  /* config options here */
   basePath,
-  // output: "export",
+  output: "export",
   assetPrefix: isProd ? "/tic-tac-toe" : "",
   env: {
     basePath,
